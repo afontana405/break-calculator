e50d21cc9a4162f68ec0f0295a1f696b0d6290a2
afontana405 <alex13579@icloud.com>
2023-10-09 14:36:37 -0500

    removed horizontal scroll bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,4 +24,10 @@ $(document).ready(function() {
             checkEndBreak();
         };
     };
+
+    $.each( $('*'), function() { 
+        if( $(this).width() > $('body').width()) {
+            console.log("Wide Element: ", $(this), "Width: ", $(this).width()); 
+        } 
+    });
 });
