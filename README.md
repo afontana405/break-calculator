c976c518195d182dc5efa8c8f14e57bbf705941c
afontana405 <alex13579@icloud.com>
2023-10-09 14:15:08 -0500

    worked on css

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # break-calculator
+
+## Description
+
+In order to help users that struggle adding 15 minutes to the current time and help suggested a more convenient time to reconvine, this program was created with the intention to solve both of these problems.
+
+## Usage
+
+![break-calculator]
+
+Deployed Website: 
+## Credits
+
+While all the coding can be traced back to me, I must acknowledge that I would have never had started this project if it were not for my teacher's inabilty to add 15 minutes to the current time.
+
+## License
+
+Please refer to the LICENSE in the repo.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,15 @@
 $(document).ready(function() {
-    var Time = dayjs().format('hh:mm');
-    var presentTime = dayjs();
     var currentTimeEl = $('#currentTime');
     var endBreakEl = $('#endBreak');
     var suggestedEndEl = $('#suggestedEnd');
+    var presentTime = dayjs();
     var newTime = presentTime.add(15, 'minute');
-    var endBreakHour = newTime.format('hh');
-    var endBreakMinutes = newTime.format('mm');
 
     $(function() {
         // displays current time
-        currentTimeEl.text('It is currently ' + Time);
+        currentTimeEl.text('It is currently ' + presentTime.format('h:mm A'));
         // displays end break time
-        endBreakEl.text('Come back from break at ' + newTime.format('hh:mm'));
+        endBreakEl.text('Come back from break at ' + newTime.format('h:mm A'));
         checkEndBreak();
     });
 
@@ -20,9 +17,9 @@ $(document).ready(function() {
     function checkEndBreak() {
         if (newTime.format('mm') % 5 === 0) {
             // displays rounded time
-            suggestedEndEl.text('Or we could round up and call it '+ newTime.format('hh:mm'));
+            suggestedEndEl.text('Or we could round up and call it '+ newTime.format('h:mm A'));
         } else {
-            // if time is not divisible by 5, round adds 1 until it is
+            // if time is not divisible by 5, adds 1 until it is
             newTime = newTime.add(1, 'minute');
             checkEndBreak();
         };
