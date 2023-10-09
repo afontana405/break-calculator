68db4f4f4d83a7e7c3d6148d47000db4f52546eb
afontana405 <alex13579@icloud.com>
2023-10-08 22:51:28 -0500

    completed suggested end time function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,30 @@
-var Time = dayjs().format('hh:mm');
-var presentTime = dayjs();
-var currentTimeEl = $('#currentTime');
-var endBreakEl = $('#endBreak');
-var suggestedEndEl = $('#suggestedEnd');
-var newTime = presentTime.add(15, 'minute');
-var endBreakHour = newTime.format('hh');
-var endBreakMinutes = newTime.format('mm');
+$(document).ready(function() {
+    var Time = dayjs().format('hh:mm');
+    var presentTime = dayjs();
+    var currentTimeEl = $('#currentTime');
+    var endBreakEl = $('#endBreak');
+    var suggestedEndEl = $('#suggestedEnd');
+    var newTime = presentTime.add(15, 'minute');
+    var endBreakHour = newTime.format('hh');
+    var endBreakMinutes = newTime.format('mm');
 
-$(function() {
-    // displays current time
-    currentTimeEl.text('It is currently ' + Time);
-    // displays end break time
-    endBreakEl.text('Come back from break at ' + endBreakHour + ':' + endBreakMinutes);
-    checkEndBreak();
-});
-
-// suggests an alternative break time and displays it
-function checkEndBreak() {
-    if (endBreakMinutes % 5 == 0) {
-        suggestedEndEl.text('Or we could round up and call it ' + endBreakHour + ':' + endBreakMinutes);
-    } else {
-        var endBreakMinutes = endBreakMinutes.add(1, 'minute');
-        console.log(endBreakMinutes);
+    $(function() {
+        // displays current time
+        currentTimeEl.text('It is currently ' + Time);
+        // displays end break time
+        endBreakEl.text('Come back from break at ' + newTime.format('hh:mm'));
         checkEndBreak();
+    });
+
+    // checks end break time and rounds it up to the nearest 5 minute
+    function checkEndBreak() {
+        if (newTime.format('mm') % 5 === 0) {
+            // displays rounded time
+            suggestedEndEl.text('Or we could round up and call it '+ newTime.format('hh:mm'));
+        } else {
+            // if time is not divisible by 5, round adds 1 until it is
+            newTime = newTime.add(1, 'minute');
+            checkEndBreak();
+        };
     };
-    return;
-};
+});
